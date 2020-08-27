c673e4a37d7c3ae61c86cc12ce0de73619e39835
Nielsen Hofmam <nielsen.hofmam@gmail.com>
2020-08-26 23:24:53 -0300

    Criando entidades e carga inicial de dados.

---
+++ SalesWebMvc/Data/EmailService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+
+    public class EmailService
+    {
+        private SeedingService _seeding;
+
+        public EmailService(SeedingService seeding)
+        {
+            _seeding = seeding;
+        }
+
+        public void Execute()
+        {
+            var x = _seeding.RetValue();
+        }
+       
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -39,14 +39,20 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                         builder.MigrationsAssembly("SalesWebMvc")));
+
+            services.AddScoped<SeedingService>();
+            services.AddScoped<EmailService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService, EmailService emailService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
+                emailService.Execute();
             }
             else
             {
